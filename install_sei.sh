1a5c6f1b73fd3e3ca99f5f5d49d447ef4371cef6
95brsu <103598894+95brsu@users.noreply.github.com>
2022-06-02 13:12:40 +0300

    Update install_sei.sh

--- install_sei.sh
+++ install_sei.sh
@@ -56,7 +56,7 @@ go version
 echo -e "\e[1m\e[32m3. Downloading and building binaries... \e[0m" && sleep 1
 # download binary
 cd $HOME
-git clone https://github.com/sei-protocol/sei-chain.git
+git clone --depth 1 --branch 1.0.1beta https://github.com/sei-protocol/sei-chain.git
 cd sei-chain
 git checkout 1.0.0beta
 go build -o build/seid ./cmd/sei-chaind
